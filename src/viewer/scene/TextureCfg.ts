b18b663bde23cbefbecebec7faaa54d07c21e3f1
xeolabs <lindsay.kay@xeolabs.com>
2022-08-02 01:01:10 +0200

    Comment tweaks

--- src/viewer/scene/TextureCfg.ts
+++ src/viewer/scene/TextureCfg.ts
@@ -26,27 +26,33 @@ export interface TextureCfg {
     image?: HTMLImageElement;
 
     /**
-     * How the texture is sampled when a texel covers more than one pixel. Supported values
-     * are {@link LinearFilter} and {@link NearestFilter}.
+     * How the texture is sampled when a texel covers more than one pixel.
+     *
+     * Supported values are {@link LinearFilter} and {@link NearestFilter}.
      */
     magFilter?: number;
 
     /**
-     * How the texture is sampled when a texel covers less than one pixel. Supported values
-     * are {@link LinearMipmapLinearFilter}, {@link LinearMipMapNearestFilter},
+     * How the texture is sampled when a texel covers less than one pixel.
+     *
+     * Supported values are {@link LinearMipmapLinearFilter}, {@link LinearMipMapNearestFilter},
      * {@link NearestMipMapNearestFilter}, {@link NearestMipMapLinearFilter}
      * and {@link LinearMipMapLinearFilter}.
      */
     minFilter?: number;
 
     /**
-     * Wrap parameter for texture coordinate *S*. Supported values are {@link ClampToEdgeWrapping},
+     * Wrap parameter for texture coordinate *S*.
+     *
+     * Supported values are {@link ClampToEdgeWrapping},
      * {@link MirroredRepeatWrapping} and {@link RepeatWrapping}.
      */
     wrapS?: number;
 
     /**
-     * Wrap parameter for texture coordinate *T*. Supported values are {@link ClampToEdgeWrapping},
+     * Wrap parameter for texture coordinate *T*.
+     *
+     * Supported values are {@link ClampToEdgeWrapping},
      * {@link MirroredRepeatWrapping} and {@link RepeatWrapping}.
      */
     wrapT?: number;
@@ -57,7 +63,9 @@ export interface TextureCfg {
     flipY?: boolean;
 
     /**
-     * Encoding format. Supported values are {@link LinearEncoding} and {@link sRGBEncoding}.
+     * Texture encoding format.
+     *
+     * Supported values are {@link LinearEncoding} and {@link sRGBEncoding}.
      */
     encoding?: number;
 

e797ad71f20c796e158cdc352769a5b34605d0a7
xeolabs <lindsay.kay@xeolabs.com>
2022-11-15 19:15:25 +0100

    Refactorings

--- _WebGLSceneModel/lib/layers/vboInstancing/triangles/TrianglesInstancingLayer.ts
+++
@@ -1,8 +0,0 @@
-import {getTrianglesInstancingRenderers} from "./TrianglesInstancingRenderers";
-import {VBOInstancingLayer, VBOInstancingLayerCfg} from "../VBOInstancingLayer";
-
-export class TrianglesInstancingLayer extends VBOInstancingLayer {
-    constructor(params: VBOInstancingLayerCfg) {
-        super(params, getTrianglesInstancingRenderers(params.sceneModel.view));
-    }
-}

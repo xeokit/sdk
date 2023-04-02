71dc589a3e94acd51c716b4cd5a17a70d01237e0
Lindsay Kay <lindsay.kay@xeolabs.com>
2023-04-02 12:03:43 +0200

    Delete rollup directory

--- rollup/rollup.gltf.config.js
+++
@@ -1,9 +0,0 @@
-import buildBaseConfig from "./rollup.base.config";
-
-const input = process.env.INPUT;
-
-const baseConfig = buildBaseConfig(input, 'gltf');
-
-export default {
-    ...baseConfig,
-}

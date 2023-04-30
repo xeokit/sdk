ba5e38c97504c7e4a852f0837503d79ff64359cc
lindsay <lindsay.kay@xeolabs.com>
2023-05-01 00:57:28 +0200

    Refactorings

--- packages/basictypes/dist/index.js
+++
@@ -1,2 +0,0 @@
-var t,e=((t={})[1e3]="BasicEntity",t[1001]="BasicAggregation",t);exports.BasicAggregation=1001,exports.BasicEntity=1e3,exports.typeCodes={BasicEntity:1e3,BasicAggregation:1001},exports.typeNames=e;
-//# sourceMappingURL=index.js.map

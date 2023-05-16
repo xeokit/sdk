12944397069f1798d4fc3ef9a37186d00dc4a853
lindsay <lindsay.kay@xeolabs.com>
2023-05-17 01:14:55 +0200

    Refactorings

--- packages/examples/src/xeokit-bundle.ts
+++ packages/examples/src/xeokit-bundle.ts
@@ -18,22 +18,22 @@ export * as constants from "@xeokit/constants";
 
 // Utility libraries
 
- export * as math from "@xeokit/math";
- export * as matrix from "@xeokit/matrix";
- export * as utils from "@xeokit/utils";
- export * as rtc from "@xeokit/rtc";
- export * as curves from "@xeokit/curves";
- export * as boundaries from "@xeokit/boundaries";
- export * as compression from "@xeokit/compression";
+export * as math from "@xeokit/math";
+export * as matrix from "@xeokit/matrix";
+export * as utils from "@xeokit/utils";
+export * as rtc from "@xeokit/rtc";
+export * as curves from "@xeokit/curves";
+export * as boundaries from "@xeokit/boundaries";
+export * as compression from "@xeokit/compression";
 //
 // // Localization
 //
- export * as locale from "@xeokit/locale";
+export * as locale from "@xeokit/locale";
 //
 // // Model definition
 //
- export * as data from "@xeokit/data";
- export * as scene from "@xeokit/scene";
+export * as data from "@xeokit/data";
+export * as scene from "@xeokit/scene";
 //
 // // Collisions, picking
 //
@@ -43,18 +43,18 @@ export * as constants from "@xeokit/constants";
 //
 // // Semantic types
 //
-// export * as basicTypes from "@xeokit/basictypes";
-// export * as cityJSONTypes from "@xeokit/cityjsontypes_1_1_3";
-// export * as ifcTypes from "@xeokit/ifctypes";
+export * as basictypes from "@xeokit/basictypes";
+export * as cityjsontypes from "@xeokit/cityjsontypes_1_1_3";
+export * as ifctypes from "@xeokit/ifctypes";
 
 // Loaders
 
 //export * as las from "@xeokit/las";
 export * as cityjson from "@xeokit/cityjson";
-// export * as dotbim from "@xeokit/dotbim";
-// export * as gltf from "@xeokit/gltf";
+export * as dotbim from "@xeokit/dotbim";
+export * as gltf from "@xeokit/gltf";
 // export * as webifc from "@xeokit/webifc";
-// export * as xkt from "@xeokit/xkt";
+export * as xkt from "@xeokit/xkt";
 
 // Viewer
 

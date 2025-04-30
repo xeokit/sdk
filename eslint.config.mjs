3f51804cc00634be81e3fc17b39519f8253d14bd
damianbobrowski <damian.bobrowski@creoox.com>
2025-04-30 13:44:59 +0200

    style: enable '@typescript-eslint/no-unused-vars' rule with warning level

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,7 +29,7 @@ export default tseslint.config(
       '@stylistic/ts/object-curly-spacing': ['error', 'always'],
       '@stylistic/ts/indent': ['error', 2],
       // '@typescript-eslint/no-explicit-any': 'off',
-      // '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
       // '@typescript-eslint/ban-ts-comment': 'off',
       // '@typescript-eslint/no-unused-expressions': 'off',
       // 'no-unused-private-class-members': 'off',

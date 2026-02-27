e0875f25aac58303b1c5cf5be342e8ac9efed1f3
xeolabs <lindsay.kay@xeolabs.com>
2026-02-27 11:52:24 +0100

    Fix buildBoxGeometry return

--- packages/sdk/src/procgen/buildBoxGeometry.ts
+++ packages/sdk/src/procgen/buildBoxGeometry.ts
@@ -96,106 +96,109 @@ export function buildBoxGeometry(cfg: {
   const ymax = ySize + centerY;
   const zmax = zSize + centerZ;
 
-  return utils.apply(cfg, {
+  return {
+    ok: true,
+    value: utils.apply(cfg, {
 
-    primitive: TrianglesPrimitive,
+      primitive: TrianglesPrimitive,
 
-    // The vertices - eight for our cube, each
-    // one spanning three array elements for X, Y, and Z
-    positions: [
+      // The vertices - eight for our cube, each
+      // one spanning three array elements for X, Y, and Z
+      positions: [
 
-      // v0-v1-v2-v3 front
-      xmax, ymax, zmax,
-      xmin, ymax, zmax,
-      xmin, ymin, zmax,
-      xmax, ymin, zmax,
+        // v0-v1-v2-v3 front
+        xmax, ymax, zmax,
+        xmin, ymax, zmax,
+        xmin, ymin, zmax,
+        xmax, ymin, zmax,
 
-      // v0-v3-v4-v1 right
-      xmax, ymax, zmax,
-      xmax, ymin, zmax,
-      xmax, ymin, zmin,
-      xmax, ymax, zmin,
+        // v0-v3-v4-v1 right
+        xmax, ymax, zmax,
+        xmax, ymin, zmax,
+        xmax, ymin, zmin,
+        xmax, ymax, zmin,
 
-      // v0-v1-v6-v1 top
-      xmax, ymax, zmax,
-      xmax, ymax, zmin,
-      xmin, ymax, zmin,
-      xmin, ymax, zmax,
+        // v0-v1-v6-v1 top
+        xmax, ymax, zmax,
+        xmax, ymax, zmin,
+        xmin, ymax, zmin,
+        xmin, ymax, zmax,
 
-      // v1-v6-v7-v2 left
-      xmin, ymax, zmax,
-      xmin, ymax, zmin,
-      xmin, ymin, zmin,
-      xmin, ymin, zmax,
+        // v1-v6-v7-v2 left
+        xmin, ymax, zmax,
+        xmin, ymax, zmin,
+        xmin, ymin, zmin,
+        xmin, ymin, zmax,
 
-      // v7-v4-v3-v2 bottom
-      xmin, ymin, zmin,
-      xmax, ymin, zmin,
-      xmax, ymin, zmax,
-      xmin, ymin, zmax,
+        // v7-v4-v3-v2 bottom
+        xmin, ymin, zmin,
+        xmax, ymin, zmin,
+        xmax, ymin, zmax,
+        xmin, ymin, zmax,
 
-      // v4-v7-v6-v1 back
-      xmax, ymin, zmin,
-      xmin, ymin, zmin,
-      xmin, ymax, zmin,
-      xmax, ymax, zmin
-    ],
+        // v4-v7-v6-v1 back
+        xmax, ymin, zmin,
+        xmin, ymin, zmin,
+        xmin, ymax, zmin,
+        xmax, ymax, zmin
+      ],
 
-    // UV coordinates for each vertex
-    uv: [
+      // UV coordinates for each vertex
+      uv: [
 
-      // v0-v1-v2-v3 front
-      1, 0,
-      0, 0,
-      0, 1,
-      1, 1,
+        // v0-v1-v2-v3 front
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1,
 
-      // v0-v3-v4-v1 right
-      0, 0,
-      0, 1,
-      1, 1,
-      1, 0,
+        // v0-v3-v4-v1 right
+        0, 0,
+        0, 1,
+        1, 1,
+        1, 0,
 
-      // v0-v1-v6-v1 top
-      1, 1,
-      1, 0,
-      0, 0,
-      0, 1,
+        // v0-v1-v6-v1 top
+        1, 1,
+        1, 0,
+        0, 0,
+        0, 1,
 
-      // v1-v6-v7-v2 left
-      1, 0,
-      0, 0,
-      0, 1,
-      1, 1,
+        // v1-v6-v7-v2 left
+        1, 0,
+        0, 0,
+        0, 1,
+        1, 1,
 
-      // v7-v4-v3-v2 bottom
-      0, 1,
-      1, 1,
-      1, 0,
-      0, 0,
+        // v7-v4-v3-v2 bottom
+        0, 1,
+        1, 1,
+        1, 0,
+        0, 0,
 
-      // v4-v7-v6-v1 back
-      0, 1,
-      1, 1,
-      1, 0,
-      0, 0
-    ],
+        // v4-v7-v6-v1 back
+        0, 1,
+        1, 1,
+        1, 0,
+        0, 0
+      ],
 
-    // Indices that organize vertices into geometric primitives (triangles)
-    // The triangles are specified in counter-clockwise winding order
-    indices: [
-      0, 1, 2,
-      0, 2, 3, // front
-      4, 5, 6,
-      4, 6, 7, // right
-      8, 9, 10,
-      8, 10, 11, // top
-      12, 13, 14,
-      12, 14, 15, // left
-      16, 17, 18,
-      16, 18, 19, // bottom
-      20, 21, 22,
-      20, 22, 23 // back
-    ]
-  });
+      // Indices that organize vertices into geometric primitives (triangles)
+      // The triangles are specified in counter-clockwise winding order
+      indices: [
+        0, 1, 2,
+        0, 2, 3, // front
+        4, 5, 6,
+        4, 6, 7, // right
+        8, 9, 10,
+        8, 10, 11, // top
+        12, 13, 14,
+        12, 14, 15, // left
+        16, 17, 18,
+        16, 18, 19, // bottom
+        20, 21, 22,
+        20, 22, 23 // back
+      ]
+    })
+  };
 }
